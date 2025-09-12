b1012a8381310d383f06878dac268ebec538c227
aguirrehdezma <aguirrehdezma@gmail.com>
2025-09-12 10:32:08 -0700

    chore(backendcacei): update settings and URL configuration

--- backendcacei/backendcacei/settings.py
+++ backendcacei/backendcacei/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'programas_educativos',
+    'criterios_desempeno',
+    'unidades_tematicas',
 ]
 
 MIDDLEWARE = [
