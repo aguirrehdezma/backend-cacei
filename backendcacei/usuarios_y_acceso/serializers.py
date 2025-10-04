31d4b51a7f40de9b1f41fd799abb84b79e9619f9
albertobetaorionis-gif <albertobetaorionis@gmail.com>
2025-10-03 19:49:34 -0700

    Actualzidacion serializers.py
    Actualización de los serializers-Alberto

--- backendcacei/usuarios_y_acceso/serializers.py
+++ backendcacei/usuarios_y_acceso/serializers.py
@@ -1,2 +1,34 @@
 from rest_framework import serializers
+from usuarios_y_acceso.models import Usuarios
+from usuarios_y_acceso.models import UsuariosProgramas
+
+# Create your serializers here.
+
+class UsuariosSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Usuarios
+        fields = [
+            'usuario_id',
+            'username',
+            'password_hash',
+            'email',
+            'nombre_completo',
+            'rol',
+            'estatus',
+            'last_login',
+            'created_at',
+            'updated_at'
+        ]
+
+
+class UsuariosProgamasSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UsuariosProgramas
+        fields = [
+            'usuario_programa_id',
+            'usuario_id',
+            'programa_id',
+            'created_at',
+            'updated_at'
+        ]
 
